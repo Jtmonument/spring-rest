c9ecd80fee08dd137012908192e670769a4c6aef
jonta <jontaylor091@gmail.com>
2022-06-10 19:41:49 -0500

    Created CommandLineRunner

---
+++ src/main/java/com/example/payroll/LoadDatabase.java
@@ -0,0 +1,19 @@
+package com.example.payroll;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LoadDatabase {
+    private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
+
+    @Bean
+    CommandLineRunner initDatabase(EmployeeRepository repo) {
+        return args -> {
+            log.info("Preloading " + repo.save(new Employee("Jonathan Taylor", "SDE")));
+        };
+    }
+}
