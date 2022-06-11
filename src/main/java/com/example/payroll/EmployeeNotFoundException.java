116ee3f5eac6c9f687d6660f65237c696a146bb4
jonta <jontaylor091@gmail.com>
2022-06-10 19:43:12 -0500

    Created EmployeeNotFoundException

---
+++ src/main/java/com/example/payroll/EmployeeNotFoundException.java
@@ -0,0 +1,7 @@
+package com.example.payroll;
+
+public class EmployeeNotFoundException extends RuntimeException {
+    EmployeeNotFoundException(Long id) {
+        super("Could not find employee " + id);
+    }
+}
